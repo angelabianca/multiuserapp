734b2bab450e9e14a5ae9accfe58e148bd7459ca
angelabianca <angelabianca@hotmail.ch>
2020-07-07 16:14:06 +0200

    new Classes etc

---
+++ src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/init/ServerInitialize.java
@@ -0,0 +1,5 @@
+package ch.zli.m223.ksh17a.angela.meixner.multiuserapp.init;
+
+public class ServerInitialize {
+
+}
---
+++ src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package ch.zli.m223.ksh17a.angela.meixner.multiuserapp.repository;
+
+public interface UserRepository {
+
+}
