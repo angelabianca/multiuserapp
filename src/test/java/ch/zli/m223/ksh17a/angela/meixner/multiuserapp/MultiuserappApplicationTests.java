7b85a12313da8af5b02dfbe92846287e80a4e5ef
angelabianca <angelabianca@hotmail.ch>
2020-07-07 15:59:12 +0200

    Test

---
+++ src/test/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/MultiuserappApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.ksh17a.angela.meixner.multiuserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiuserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
