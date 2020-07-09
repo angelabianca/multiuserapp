4665a2153c5515f04d4a8be7824a46e8fd5eb0a4
angelabianca <angelabianca@hotmail.ch>
2020-07-09 11:27:41 +0200

    updated version

--- src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/controller/rest/UserRestController.java
+++ src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/controller/rest/UserRestController.java
@@ -1,5 +1,61 @@
 package ch.zli.m223.ksh17a.angela.meixner.multiuserapp.controller.rest;
 
-public class UserRestController {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.model.memo.MemoDto;
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.model.user.UserDto;
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.service.memo.MemoService;
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.service.user.UserService;
+
+@RestController
+public class UserRestController {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	MemoService memoService;
+	
+	@GetMapping("/rest/users")
+	List<UserDto> getUserList() {
+		
+		return userService.getAllUsers();
+	}
+	
+	@GetMapping("/rest/users/{id}")
+	UserDto getUser(@PathVariable("id") Long id) {
+		
+		return new UserDto(userService.getUserById(id));
+	}
+	
+	@PostMapping("/rest/users")
+	UserDto createUser(@RequestBody UserDto userDto) {
+		
+		return new UserDto(userService.createUser(userDto.name));
+	}
+	
+	@DeleteMapping("/rest/users/{id}")
+	void deleteUser(@PathVariable("id") Long id) {
+		
+		userService.deleteUser(id);
+	}
+	
+	@GetMapping("/rest/users/{id}/memos")
+	List<MemoDto> getMemos(@PathVariable("id") Long userId) {
+		
+		return memoService.getUserMemos(userId);
+	}
+	
+	@PostMapping("/rest/users/{id}/memos")
+	MemoDto createMemo(@RequestBody MemoDto memoDto) {
+		
+		return memoService.createMemo(memoDto);
+	}
 }
--- src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/init/ServerInitialize.java
+++ src/main/java/ch/zli/m223/ksh17a/angela/meixner/multiuserapp/init/ServerInitialize.java
@@ -1,5 +1,37 @@
 package ch.zli.m223.ksh17a.angela.meixner.multiuserapp.init;
 
-public class ServerInitialize {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.model.user.User;
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.repository.memo.MemoRepository;
+import ch.zli.m223.ksh17a.angela.meixner.multiuserapp.repository.user.UserRepository;
+
+@Component
+public class ServerInitialize implements ApplicationRunner {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	MemoRepository memoRepository;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		User user1, user2, user3, user4;
+		
+		user1 = userRepository.createUser("user1");
+		user2 = userRepository.createUser("user2");
+		user3 = userRepository.createUser("user3");
+		user4 = userRepository.createUser("user4");
+		
+		memoRepository.createMemo(user1, "note for user 1");
+		memoRepository.createMemo(user1, "another note for user 1");
+		memoRepository.createMemo(user2, "note for user 2");
+		memoRepository.createMemo(user3, "note for user 3");
+		memoRepository.createMemo(user4, "note for user 4");
+	}
 }
